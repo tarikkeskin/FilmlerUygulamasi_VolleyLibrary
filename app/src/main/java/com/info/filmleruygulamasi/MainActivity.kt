dffb10127daa3b2050f2e0f8ef371f7240198832
tarikkeskin <tarik.keskin007@gmail.com>
2022-07-05 18:06:26 +0300

    Volley setup & get all films

--- app/src/main/java/com/info/filmleruygulamasi/MainActivity.kt
+++ app/src/main/java/com/info/filmleruygulamasi/MainActivity.kt
@@ -3,7 +3,13 @@ package com.info.filmleruygulamasi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,16 +26,37 @@ class MainActivity : AppCompatActivity() {
         kategoriRv.setHasFixedSize(true)
         kategoriRv.layoutManager = LinearLayoutManager(this)
 
-        kategoriListe = ArrayList()
+        tumKategoriler()
+    }
 
-        val k1 = Kategoriler(1,"Komedi")
-        val k2 = Kategoriler(2,"Bilim Kurgu")
+    fun tumKategoriler(){
+        var url = "http://kasimadalan.pe.hu/filmler/tum_kategoriler.php"
 
-        kategoriListe.add(k1)
-        kategoriListe.add(k2)
+        val istek = StringRequest(Request.Method.GET,url, { cevap ->
 
-        adapter = KategoriAdapter(this,kategoriListe)
+            try {
+                kategoriListe = ArrayList()
 
-        kategoriRv.adapter = adapter
+                val jsonObject = JSONObject(cevap)
+                val kategoriler = jsonObject.getJSONArray("kategoriler")
+
+                for (i in 0 until kategoriler.length()){
+                    val k = kategoriler.getJSONObject(i)
+
+                    val kategori = Kategoriler(k.getInt("kategori_id"),k.getString("kategori_ad"))
+
+                    kategoriListe.add(kategori)
+                }
+                adapter = KategoriAdapter(this,kategoriListe)
+
+                kategoriRv.adapter = adapter
+
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+
+        }, {  })
+
+        Volley.newRequestQueue(this@MainActivity).add(istek)
     }
 }
