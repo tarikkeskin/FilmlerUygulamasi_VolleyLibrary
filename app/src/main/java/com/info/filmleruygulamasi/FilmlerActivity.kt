94d6f559d4d5889037ff6f21fd3f2408d525164f
tarikkeskin <tarik.keskin007@gmail.com>
2022-07-05 18:22:34 +0300

    POST operation on volley

--- app/src/main/java/com/info/filmleruygulamasi/FilmlerActivity.kt
+++ app/src/main/java/com/info/filmleruygulamasi/FilmlerActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_filmler.*
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.lang.Exception
 
 class FilmlerActivity : AppCompatActivity() {
 
@@ -24,22 +29,56 @@ class FilmlerActivity : AppCompatActivity() {
         filmlerRv.setHasFixedSize(true)
         filmlerRv.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
 
-        filmListe = ArrayList()
+        tumFilmlerByKategoriId(kategori.kategori_id)
 
-        val k = Kategoriler(1,"Dram")
-        val y = Yonetmenler(1,"Quentin Tarantino")
+    }
 
-        val f1 = Filmler(1,"Django",2008,"django",k,y)
-        val f2 = Filmler(2,"Inception",2009,"inception",k,y)
-        val f3 = Filmler(3,"The Pianist",2010,"thepianist",k,y)
+    fun tumFilmlerByKategoriId(kategori_id:Int){
+        var url = "http://kasimadalan.pe.hu/filmler/filmler_by_kategori_id.php"
 
-        filmListe.add(f1)
-        filmListe.add(f2)
-        filmListe.add(f3)
+        val istek = object: StringRequest(Request.Method.POST,url, { cevap ->
 
-        adapter = FilmlerAdapter(this,filmListe)
+            try {
+                filmListe = ArrayList()
 
-        filmlerRv.adapter = adapter
+                val jsonObject = JSONObject(cevap)
+                val filmler = jsonObject.getJSONArray("filmler")
 
+                for (i in 0 until filmler.length()){
+                    val f = filmler.getJSONObject(i)
+
+                    val k = f.getJSONObject("kategori")
+                    val kategori = Kategoriler(k.getInt("kategori_id"),k.getString("kategori_ad"))
+
+
+                    val y = f.getJSONObject("yonetmen")
+                    val yonetmen = Yonetmenler(y.getInt("yonetmen_id"),y.getString("yonetmen_ad"))
+
+                    val film = Filmler(f.getInt("film_id")
+                                        , f.getString("film_ad")
+                                        , f.getInt("film_yil")
+                                        ,f.getString("film_resim")
+                                        ,kategori
+                                        ,yonetmen)
+                    filmListe.add(film)
+                }
+
+                adapter = FilmlerAdapter(this,filmListe)
+
+                filmlerRv.adapter = adapter
+
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+
+        }, {  }){
+            override fun getParams(): MutableMap<String, String>? {
+                val params = HashMap<String,String>()
+                params["kategori_id"] = kategori_id.toString()
+                return params
+            }
+        }
+
+        Volley.newRequestQueue(this@FilmlerActivity).add(istek)
     }
 }
